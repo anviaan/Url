fb8fa76ccf92ec318fbaa7b355d21fffb31bfc1f
andre-carbajal <carbajalvargasandre@gmail.com>
2024-08-05 21:58:51 -0500

    Url validation

--- src/main/java/net/andrecarbajal/url_shortener/domain/url/UrlRecord.java
+++ src/main/java/net/andrecarbajal/url_shortener/domain/url/UrlRecord.java
@@ -1,8 +1,9 @@
 package net.andrecarbajal.url_shortener.domain.url;
 
 import jakarta.validation.constraints.NotBlank;
+import org.hibernate.validator.constraints.URL;
 
 public record UrlRecord(
-        @NotBlank String originalUrl
+        @NotBlank @URL String originalUrl
 ) {
 }
